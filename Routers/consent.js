26fa7360167f6dea6dfb877f0f45de3ea4629c7b
Syed Abdul Samad <abdul.samad>
2020-12-20 18:29:36 +0200

    Get consent from db on GET request.

--- Routers/consent.js
+++ Routers/consent.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const router = express.Router();
+
 const {
     mongoose,
 } = require("../modules/dbConnectivity")
 
-router.get("/", (req, res) => {
-    console.log("Coming here");
-    res.send("Consent Get working nicely now ");
+const consentSchema = new mongoose.Schema({
+    category: String,
+    version: Number,
+    text: String,
+    age: Number,
+    defaultStatus: Boolean,
+    readOnly: Boolean
+});
+
+router.get("/", async (req, res) => {
+
+    const Consent = mongoose.model("Consents", consentSchema);
+    try {
+        console.log("Calling the fetch...");
+        const result = await Consent.find();
+        res.status(200).send(result);
+
+    } catch (error) {
+        console.log("Failed to fetch...");
+        res.status(400).send(new Error(error.errors.message));
+    }
 });
 
 module.exports = router;
