409737f24cb5aac71548b582e9797b3da296e078
Syed Abdul Samad <abdul.samad>
2021-01-06 15:27:36 +0200

    Added custom DOB validation.

--- Routers/user.js
+++ Routers/user.js
@@ -4,26 +4,44 @@ import joi from "joi";
 
 const router = express.Router();
 
+
 const schema = joi.object({
     name: joi.string().required().min(3).max(100),
     email: joi.string().required().email(),
-    dob: joi.date().required()
+    DOB: joi.date().required()
 })
 
 router.post("/", async (req, res) => {
 
-    console.log("Users Router");
     const userBody = req.body;
-
     const validationResult = schema.validate(userBody);
     if (validationResult.error) {
-        console.log("Joi validation error");
-        res.status(404).send(validationResult.error.message);
+        res.status(400).send(validationResult.error.message);
         return;
     }
+
+    const DOB = new Date(userBody.DOB);
+    if (validateDate(DOB) == false) {
+        res.status(400).send("DOB should be in format YYYY-MM-DD and must be > 18 years and < 100 years");
+        return;
+    }
+
     console.log("User body is :", JSON.stringify(userBody));
-    res.send(userBody);
+    res.send(DOB);
 });
+
+
+function validateDate(DOB) {
+
+    const now = new Date();
+    const maxAllowed = (new Date()).setUTCFullYear(now.getUTCFullYear() - 18);
+    const minAllowed = (new Date()).setUTCFullYear(now.getUTCFullYear() - 100);
+
+    const maxDOB = new Date(maxAllowed);
+    const minDOB = new Date(minAllowed);
+
+    return (DOB && DOB >= minDOB && DOB < maxDOB)
+}
 export {
     router as usersRouter
 }
